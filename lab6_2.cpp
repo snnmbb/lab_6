f27220181b71f8964d9170f8ecfd6c79b32ea485
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2020-12-25 17:03:00 +0700

    edit code

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,41 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+    int id;
+    string movie;
+    string day;
+    string sentence;
+
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?"<<"\n";
+    cout<<"?????: ";
+    getline(cin,name);
+    cout<<"Fahsai: Wow!!! "<< name <<" is a really cool name."<<"\n";
+    cout<<"Fahsai: I think you are an Engineering student. What is your student ID?"<<"\n";
+    cout<<name<<": ";
+    cin>>id;
+    cout<<"Fahsai: I think you may be GEAR "<< (id/10000000)-12 <<"." <<" I have a free movie ticket for you."<<"\n";
+    cout<<"Fahsai: Let's go to the cinema together!!!"<<"\n"<<"Fahsai: What movie do you want to watch?"<<"\n";
+    cout<<name<<": ";
+
+    cin.ignore();
+    getline(cin,movie);
+    cout<<"Fahsai: So....which day are you free to go with me?"<<"\n";
+    cout<<name<<": ";
+    //cin.ignore();
+    getline(cin,day);
+
+    cout<<"Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watching "<< movie<<" with you."<<"\n";
+    cout<<name<<": ";
+    cin.ignore();
+    getline(cin,sentence);
+    cout<<"Fahsai: 555+ see you "<< day <<". Bye Bye " <<'\\'<<"(^ ^)/";
+}
+
